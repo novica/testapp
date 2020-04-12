91b8abf8ec6467746a82d3eb4a66d0b3957f5c1c
novica <nnovica@gmail.com>
2020-04-12 15:40:12 +0200

    basic structure of the dashboard

--- R/app_ui.R
+++ R/app_ui.R
@@ -3,17 +3,74 @@
 #' @param request Internal parameter for `{shiny}`. 
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @import shinydashboard
 #' @noRd
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # List the first level UI elements here 
-    fluidPage(
-      h1("testapp")
+  dashboardPage(    
+    dashboardHeader(title = "test app"),
+    dashboardSidebar(
+      sidebarMenu(id = "sidebar",
+        menuItem("Dataset 1", tabName = "dataset1", icon = icon("chart-bar")),
+        menuItem("Dataset 2", tabName = "dataset2", icon = icon("chart-area"))
+      )
+    ),
+      
+    dashboardBody(
+      tabItems(
+        tabItem(tabName = "dataset1",
+                h2("Dataset Random Lab Values Info 2020"),
+                fluidRow(
+                  box(plotOutput("plot1")),
+                  
+                  box(
+                    "Box content here", br(), "More box content",
+                    sliderInput("slider", "Slider input:", 1, 100, 50),
+                    textInput("text", "Text input:")
+                  )
+                ),
+                fluidRow(
+                  box(plotOutput("plot2")),
+                  
+                  box(
+                    "Box content here", br(), "More box content",
+                    sliderInput("slider", "Slider input:", 1, 100, 50),
+                    textInput("text", "Text input:")
+                  )
+                )
+        ),
+        
+        tabItem(tabName = "dataset2",
+                h2("Dataset Patient Level Info 2020"),
+                fluidRow(
+                  box(plotOutput("plot3")),
+                  
+                  box(
+                    "Box content here", br(), "More box content",
+                    sliderInput("slider", "Slider input:", 1, 100, 50),
+                    textInput("text", "Text input:")
+                  )
+                ),
+                fluidRow(
+                  box(plotOutput("plot4")),
+                  
+                  box(
+                    "Box content here", br(), "More box content",
+                    sliderInput("slider", "Slider input:", 1, 100, 50),
+                    textInput("text", "Text input:")
+                  )
+                )
+        )
+      )
     )
   )
+ )
 }
+
+
 
 #' Add external Resources to the Application
 #' 
