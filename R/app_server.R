f7a8790e10f4f17f6071527125ad9f7e4b3811a1
novica <nnovica@gmail.com>
2020-04-14 19:23:18 +0200

    basic functionality works

--- R/app_server.R
+++ R/app_server.R
@@ -6,7 +6,7 @@
 #' @noRd
 app_server <- function( input, output, session ) {
   # List the first level callModules here
-
+  
   ds_head <- reactive({
     validate(need(!is.null(input$dataset), 
                   "Hi there, please select dataset."))
@@ -17,60 +17,70 @@ app_server <- function( input, output, session ) {
     }
     
     return(x)    
-    })
+  })
   
   choose_vars <- reactive({
     validate(need(!is.null(input$dataset), "Waiting for dataset..."))
     
     if (input$dataset == "dataset1") {
-    x <- tagList( selectInput(inputId =  "chooseVar1", 
-                label = "Variables", choices =  c("AVAL", "BMRKR1")),
-    
-    selectInput(inputId =  "chooseFill1", 
-                label =  "Variables", choices = c("AVISIT", "LBTESTCD")),
-    
-    actionButton(inputId = "makePlot2", label = "Plot")
+      x <- tagList( selectInput(inputId =  "chooseVar1", 
+                                label = "Variables", choices =  c("AVAL", "BMRKR1")),
+                    
+                    selectInput(inputId =  "chooseFill1", 
+                                label =  "Variables", choices = c("AVISIT", "LBTESTCD")),
+                    
+                    actionButton(inputId = "makePlot1", label = "Plot")
       )
     } else {
       x <- tagList(selectInput(inputId =  "chooseVar2", 
-                       label = "Variables", choices =  c("AGE", "RACE")),
-      
-      selectInput(inputId =  "chooseFill2", 
-                  label =  "Variables", choices = c("SEX", "ACTARM")),
-      
-      actionButton(inputId = "makePlot2", label = "Plot")
+                               label = "Variables", choices =  c("AGE", "RACE")),
+                   
+                   selectInput(inputId =  "chooseFill2", 
+                               label =  "Variables", choices = c("SEX", "ACTARM")),
+                   
+                   actionButton(inputId = "makePlot2", label = "Plot")
       )
     }
     
     return(x)
   })
- 
   
+  last_clicked_button <- reactiveVal(NA)
   
-  buttonstates <- reactiveValues(one = 0)
+  observeEvent(input$makePlot1, {
+    last_clicked_button("p1")
+  })
+  
+  observeEvent(input$makePlot2, {
+    last_clicked_button("p2")
+  })
   
   #' @import ggplot2
   whattoplot <- reactive({
-    req(input$makePlot1, input$makePlot2)
-    if (input$makePlot1 != buttonstates$one) {
-      ggplot(ds_head) +
-        aes(x = input$chooseVar1, fill = input$chooseFill1, color = input$chooseFill1) +
-        geom_histogram(position = "dodge", bins = 15)
-      } else { 
-        ggplot(ds_head) +
-          aes(x = input$chooseVar2, fill = input$chooseFill2, color = input$chooseFill2) +
-          geom_histogram(position = "dodge", bins = 15)
-      }
+    req(last_clicked_button())
+    if (last_clicked_button() == "p1") {
+      x <- ggplot(dataset1) +
+        aes(x = !!rlang::sym(input$chooseVar1), 
+            fill = !!rlang::sym(input$chooseFill1), 
+            color = !!rlang::sym(input$chooseFill1)) +
+      geom_histogram(position = "dodge", bins = 15, stat="count")
+    } 
+    
+    if (last_clicked_button() == "p2") { 
+      x <- ggplot(dataset2) +
+        aes(x = !!rlang::sym(input$chooseVar2), 
+            fill = !!rlang::sym(input$chooseFill2), 
+            color = !!rlang::sym(input$chooseFill2)) +
+        geom_histogram(position = "dodge", bins = 15, stat="count")
+    }
     
     return(x)
   })
   
-
-
   output$headTable <- renderTable({ ds_head() })
   
   output$makePlot <- renderUI({ choose_vars() })
-    
+  
   output$plot <- renderPlot({
     req(whattoplot())
   })
