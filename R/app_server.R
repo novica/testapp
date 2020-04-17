75aaedb0822f8903ec2e119e09a9b79a0c5742b9
novica <nnovica@gmail.com>
2020-04-17 12:31:21 +0200

    make sure datasets are properly loaded

--- R/app_server.R
+++ R/app_server.R
@@ -4,20 +4,25 @@
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
+#' 
+
+dataset_1 <- dataset1
+dataset_2 <- dataset2
+
 app_server <- function( input, output, session ) {
   # List the first level callModules here
   
   dropdown_choices <- reactive({
     if (input$vars == "dataset1") {
-      x <- colnames(dataset1)[3]
-      y <- colnames(dataset1)[8]
+      x <- colnames(dataset_1)[3]
+      y <- colnames(dataset_1)[8]
       c <- y # probably redundant, but for clarity
       var_list <- list("first_var" = x, "second_var" = y, "third_var" = c)
     }
     
     if (input$vars == "dataset2") {
-      x <- colnames(dataset2)[3]
-      f <- colnames(dataset2)[c(4,5)]
+      x <- colnames(dataset_2)[3]
+      f <- colnames(dataset_2)[c(4,5)]
       c <- f # probably redundant, but for clarity
       var_list <- list("first_var" = x, "first_var" = f, "third_var" = c)
     }
@@ -27,16 +32,16 @@ app_server <- function( input, output, session ) {
   #' @import ggplot2
   what_to_plot <- eventReactive(input$plotButton, {
     
-    first_var <- rlang::sym(from_module()[[1]])
-    second_var <- rlang::sym(from_module()[[2]])
-    third_var <- rlang::sym(from_module()[[3]])
+    first_var <- sym(from_module()[[1]])
+    second_var <- sym(from_module()[[2]])
+    third_var <- sym(from_module()[[3]])
     
     
     if (input$vars == "dataset1") {
-    plt <- ggplot(dataset1) +
+    plt <- ggplot(dataset_1) +
       aes( x = !!first_var, y = !!second_var, color = !!third_var ) +
       geom_point()
-    } else plt <- ggplot(dataset2) +
+    } else plt <- ggplot(dataset_2) +
       aes( x = !!first_var, fill = !!second_var, color = !!third_var) +
       geom_histogram(position = "dodge", bins = 15)
     
