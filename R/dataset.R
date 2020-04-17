c72d4af36279b88372bcc34b3deefc331d8b3f9c
novica <nnovica@gmail.com>
2020-04-17 10:44:46 +0200

    cleaning up after devtools::check()

---
+++ R/dataset.R
@@ -0,0 +1,23 @@
+#' Random_LabValuesInfo_2020.tsv.
+#'
+#' A dataset containing sample medical data.
+#' 
+#'
+#' @format Sample dataset 1
+#' \describe{
+#'
+#' }
+#' @source \url{https://github.com/fontes-lab/RShinyProgrammingExercise}
+"dataset1"
+
+#' Random_PatientLevelInfo_2020.tsv.
+#'
+#' A dataset containing sample medical data.
+#' 
+#'
+#' @format Sample dataset 2
+#' \describe{
+#'
+#' }
+#' @source \url{https://github.com/fontes-lab/RShinyProgrammingExercise/}
+"dataset2"
--- R/fct_helpers.R
+++ R/fct_helpers.R
@@ -1,6 +1,9 @@
 ##### --- helpers ------- #####
-#' @export make_dropdown
-#' @noRd 
+#' Helper function for preparing dropdown menus
+#' @name make_dropdown
+#' @param drop_id,drop_label,drop_choices selectize input elements
+#' 
+#' @export
 
 
 make_dropdown <- function(drop_id, drop_label, drop_choices) {
